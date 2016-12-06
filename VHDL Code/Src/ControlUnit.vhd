bd8f79dcda2a326286826e074cbfcfc409d65aa6
pkhushdil <psk287@nyu.edu>
2016-12-05 21:00:07 -0500

    Tested not working
    Resolve immediate value problem

--- VHDL Code/Src/ControlUnit.vhd
+++ VHDL Code/Src/ControlUnit.vhd
@@ -1,12 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.std_logic_arith.all;
 
 entity ControlUnit is
     Port (Clk         : in STD_LOGIC;
     	 Instruction : in STD_LOGIC_VECTOR(31 DOWNTO 0); 
-    	   Opcode    : in   STD_LOGIC_VECTOR (5 downto 0);
-   read_data1_Branch : in  STD_LOGIC_VECTOR (31 downto 0);
-   read_data2_Branch : in  STD_LOGIC_VECTOR (31 downto 0);
+    	   
            PC        : in  STD_LOGIC_VECTOR (31 downto 0);
            ALUOp     : out   STD_LOGIC_VECTOR (2 downto 0);
            NextPC    : out  STD_LOGIC_VECTOR (31 downto 0);
@@ -15,9 +16,8 @@ entity ControlUnit is
            WriteEn   : out  STD_LOGIC;
            DMemRead  : out  STD_LOGIC;
            DMemWrite : out  STD_LOGIC;
-		   Jump      : out  STD_LOGIC;
-		   BranchNE  : out  STD_LOGIC;
-		   BranchLT  : out  STD_LOGIC;
+		     BranchNE  : out  STD_LOGIC;
+			  BranchLT  : out  STD_LOGIC;
            Branch    : out  STD_LOGIC );
 end ControlUnit;
 
@@ -26,15 +26,21 @@ architecture Behavioral of ControlUnit is
 	SIGNAL R_type, LWD, SWD, BEQ, ADDI, SUBI, ANDI, ORI, BNE, BLT, SHL, SHR : STD_LOGIC;
 	SIGNAL A_input, B_input                     : STD_LOGIC_VECTOR (31 DOWNTO 0);
 	SIGNAL ALU_Op                               : STD_LOGIC_VECTOR(2 DOWNTO 0 );
+	SIGNAL Immediate_value_initial                      : STD_LOGIC_VECTOR(15 DOWNTO 0);
 	SIGNAL Immediate_value                      : STD_LOGIC_VECTOR(15 DOWNTO 0);
 	SIGNAL read_register_1_address_Branch		: STD_LOGIC_VECTOR(4 DOWNTO 0 );
 	SIGNAL read_register_2_address_Branch		: STD_LOGIC_VECTOR(4 DOWNTO 0 );
 	SIGNAL PCIncby1                             : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL JumpAddress                          : STD_LOGIC_VECTOR(25 DOWNTO 0);
+	signal read_data1_Branch :   STD_LOGIC_VECTOR (31 downto 0);
+   signal read_data2_Branch :   STD_LOGIC_VECTOR (31 downto 0);
+   signal Jump      :   STD_LOGIC;
+	SIGNAL Opcode    :   STD_LOGIC_VECTOR (5 downto 0);
+   SIGNAL  numeric_immediate :INTEGER RANGE 0 TO 65536;
+	SIGNAL  numeric_immediate1 :INTEGER RANGE 0 TO 65536;
+	 TYPE register_file IS ARRAY ( 0 TO 31 ) OF STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 
-    TYPE register_file IS ARRAY ( 0 TO 31 ) OF STD_LOGIC_VECTOR( 31 DOWNTO 0 );
-
-	Signal Reg_array: register_file := (X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",
+	Signal Reg_array: register_file := (X"00000000",X"00000001",X"00000001",X"00000000",X"00000000",X"00000000",
 								        X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",
 								        X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",
 								        X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",X"00000000",
@@ -43,20 +49,27 @@ architecture Behavioral of ControlUnit is
 
 
 
+
+
+
+
 begin
 
-	Immediate_value <= Instruction( 15 DOWNTO 0 );
+	Immediate_value_initial <= Instruction( 15 DOWNTO 0 );
 	JumpAddress <= Instruction( 25 DOWNTO 0);
 
 	read_register_1_address_Branch 	<= Instruction( 25 DOWNTO 21 );
    	read_register_2_address_Branch 	<= Instruction( 20 DOWNTO 16 );
+		opcode<=Instruction(31 downto 26);
+		numeric_immediate<=conv_integer(Immediate_value_initial);
+		numeric_immediate1<=NUMERIC_IMMEDIATE - 65536;
+		
+   	Immediate_value <= conv_std_logic_vector((NUMERIC_IMMEDIATE - 65536), 16)  WHEN Immediate_value_initial(15) = '1'
+			   ELSE     Immediate_value_initial;
 
-   	Immediate_value <= (Immediate_value-65536)  WHEN Immediate_value(15) = '1'
-			   ELSE     Immediate_value;
+   	read_data1_Branch <= reg_array( CONV_INTEGER(read_register_1_address_Branch));  -- Read Register Rs for Branch
 
-   	read_data1_Branch <= register_array( CONV_INTEGER(read_register_1_address_Branch));  -- Read Register Rs for Branch
-
-	read_data2_Branch <= register_array(CONV_INTEGER(read_register_2_address_Branch));   -- Read Register Rt for Branch
+	read_data2_Branch <= reg_array(CONV_INTEGER(read_register_2_address_Branch));   -- Read Register Rt for Branch
 
 	PCIncby1 <= PC + '1';
 
@@ -82,7 +95,7 @@ begin
     begin
     	
 		If (Clk'EVENT AND Clk = '1') then
-    		If ((BEQ ='1' and (A_input=B_input)) or (BLT ='1' and (A_input < B_input)) or (BNE ='1' and (A_input != B_input))) then
+    		If ((BEQ ='1' and (A_input=B_input)) or (BLT ='1' and (A_input < B_input)) or (BNE ='1' and (A_input /= B_input))) then
     			NextPC <= PCIncby1 + Immediate_value;
     		Elsif (Jump = '1') then
     			NextPC <= PCIncby1(31 DOWNTO 26) & JumpAddress;
@@ -94,11 +107,11 @@ begin
     End Process;
 
 
-	Process (R_type, ADDI, SUBI, ANDI, ORI, LWD, SWD, )
+	Process (R_type, ADDI, SUBI, ANDI, ORI, LWD, SWD )
 	begin
 		If (R_type='1') then
 		ALU_Op <= Instruction(2 downto 0);
-		ELSIF((ADDI='1' | LWD ='1' | SWD ='1') and R_type='0') then
+		ELSIF((ADDI='1' or LWD ='1' or SWD ='1') and R_type='0') then
 			ALU_Op <= "000";
 		ELSIF (SUBI='1'and R_type='0') then
 			ALU_Op <= "001";
