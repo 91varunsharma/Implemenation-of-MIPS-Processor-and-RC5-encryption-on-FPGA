52e97e9305b366440e1e5b512ec057ad0c2ad5d3
Varun <vs1638@nyu.edu>
2016-12-10 05:54:01 -0500

    1st test Failed!!

--- VHDL Code/RC5/Data_output.vhd
+++ VHDL Code/RC5/Data_output.vhd
@@ -21,7 +21,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 package datamemory is
-        type data_output is array(0 to 31) of std_logic_vector(31 downto 0);
+        type data_output is array(0 to 113) of std_logic_vector(31 downto 0);
 end package;
 
 
