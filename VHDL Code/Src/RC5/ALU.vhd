1f172853036b3ec6b874aeb6d65002724d5358e0
addy1110 <addy.1110@gmail.com>
2016-12-11 23:23:16 -0500

    updated ALU.vhd

--- VHDL Code/Src/RC5/ALU.vhd
+++ VHDL Code/Src/RC5/ALU.vhd
@@ -19,9 +19,9 @@ use IEEE.STD_LOGIC_SIGNED.ALL;
 entity ALU is
     Port (  read_data1 : in  STD_LOGIC_VECTOR (31 downto 0);
             read_data2 : in  STD_LOGIC_VECTOR (31 downto 0);
-		    SignEx     : in  STD_LOGIC_VECTOR (31 downto 0);
+            SignEx     : in  STD_LOGIC_VECTOR (31 downto 0);
             ALUOp      : in  std_logic_vector(2 downto 0);
-			ALUSrc     : in  std_logic;
+            ALUSrc     : in  std_logic;
             ALU_Result : out STD_LOGIC_VECTOR (31 downto 0));
 end ALU;
 
@@ -37,7 +37,7 @@ begin
 	A_input <= read_data1;
 	
 	B_input <= read_data2 when ALUSrc = '0'
-	  Else     SignEx;
+	        Else     SignEx;
 
 	shift_left  <= to_stdlogicvector(to_bitvector(A_input) sll conv_integer(unsigned(B_input(4 downto 0))));
 	shift_right <= to_stdlogicvector(to_bitvector(A_input) srl conv_integer(unsigned(B_input(4 downto 0))));
