7383fb1f631d4c1c5bc835d9040aa65ba42c8509
addy1110 <addy.1110@gmail.com>
2016-12-12 00:52:17 -0500

    updated vhdl src

--- VHDL Code/Src/RC5/Data_output.vhd
+++ VHDL Code/Src/RC5/Data_output.vhd
@@ -3,13 +3,13 @@
 -- Project      :   NYU-6463 MIPS Processor
 -- Create Date  :   12/10/2016
 -- Design       :   DMem Output Package
---
+-- Description  :   VHDL code tha contains the data memory package
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 package datamemory is
-        type data_output is array(0 to 127) of std_logic_vector(31 downto 0);
+    type data_output is array(0 to 127) of std_logic_vector(31 downto 0);
 end package;
 
 
--- VHDL Code/Src/RC5/IFetch.vhd
+++ VHDL Code/Src/RC5/IFetch.vhd
@@ -1,58 +1,31 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    18:12:06 12/05/2016 
--- Design Name: 
--- Module Name:    IFetch - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- Author       :   Starboyz
+-- Project      :   NYU-6463 MIPS Processor
+-- Create Date  :   12/05/2016
+-- Design       :   Instruction Fetch
+-- FileName     :   IFetch.vhd
+-- Description  :   VHDL code that fetches an instruction
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 Entity IFetch is
-    Port (
-           NextPC      : in  STD_LOGIC_VECTOR (31 downto 0);
-           --PC          : out  STD_LOGIC_VECTOR (31 downto 0);
-           Instruction : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port (  NextPC      :   in  STD_LOGIC_VECTOR (31 downto 0);
+            Instruction :   out STD_LOGIC_VECTOR (31 downto 0));
 End IFetch;
 
 
 architecture Behavioral of IFetch is
     
---SIGNAL Program_Counter : STD_LOGIC_VECTOR (31 downto 0);
--- change array index to 1023 later
-Type IMemory IS ARRAY (0 to 20) of STD_LOGIC_VECTOR(31 downto 0);
+    Type IMemory IS ARRAY (0 to 20) of STD_LOGIC_VECTOR(31 downto 0);
 
- CONSTANT IMem : IMemory:=IMemory'(X"04010002",X"0403000a",X"0404000e",X"04050002",X"20640002",X"20630001",X"00642011",
-X"08040001",X"00622012",X"0c44000a",X"00622013",X"1c620001",X"1044000a",X"00622014",
-X"1444000a",X"1844000a",X"2805fffe",X"24850000",X"2c850000",X"30000014",X"fc000000");          
+    CONSTANT IMem : IMemory:=IMemory'(X"04010002",X"0403000a",X"0404000e",X"04050002",X"20640002",X"20630001",
+    X"00642011",X"08040001",X"00622012",X"0c44000a",X"00622013",X"1c620001",X"1044000a",X"00622014",
+    X"1444000a",X"1844000a",X"2805fffe",X"24850000",X"2c850000",X"30000014",X"fc000000");
      
 begin
-   
    Instruction <= IMem(conv_integer(NextPC));
-   --PC <= NextPC;
-
 End Behavioral;
 
