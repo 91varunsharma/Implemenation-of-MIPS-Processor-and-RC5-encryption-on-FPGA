f10b4cba49d6188ad90bf5d517a71e0035aa5724
pkhushdil <psk287@nyu.edu>
2016-12-11 22:42:41 -0500

    Final working module with control of clock

--- VHDL Code/Src/RC5ButtonClock/TopModule.vhd
+++ VHDL Code/Src/RC5ButtonClock/TopModule.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-USE IEEE.STD_LOGIC_SIGNED.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 use work.datamemory.all;
 use work.register_file.all;
@@ -9,12 +9,13 @@ use work.register_file.all;
 
 --TYPE register_file IS ARRAY ( 0 TO 31 ) OF STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 entity TopModule is
-    Port ( SW 			: in  STD_LOGIC_VECTOR (15 downto 0);
-           BTN 			: in  STD_LOGIC_VECTOR (4 downto 0)
-       --    CLK 			: in  STD_LOGIC;
---           LED 			: out  STD_LOGIC_VECTOR (15 downto 0);
---           SSEG_CA 		: out  STD_LOGIC_VECTOR (7 downto 0);
---           SSEG_AN 		: out  STD_LOGIC_VECTOR (7 downto 0)
+    Port ( 
+			  SW 			: in  STD_LOGIC_VECTOR (15 downto 0);
+           BTN 			: in  STD_LOGIC_VECTOR (4 downto 0);
+           CLK 			: in  STD_LOGIC;
+           LED 			: out  STD_LOGIC_VECTOR (15 downto 0);
+           SSEG_CA 		: out  STD_LOGIC_VECTOR (7 downto 0);
+           SSEG_AN 		: out  STD_LOGIC_VECTOR (7 downto 0)
 			  );
 end TopModule;
 
@@ -34,6 +35,7 @@ component ControlUnit
 			  skip		: in	 STD_LOGIC;
 			  Read_Data1: in   STD_LOGIC_VECTOR (31 downto 0);
 			  Read_Data2: in   STD_LOGIC_VECTOR (31 downto 0);
+			  start		:in STD_LOGIC;
            ALUOp     : out  STD_LOGIC_VECTOR (2 downto 0);
            NextPC    : out  STD_LOGIC_VECTOR (31 downto 0);
            Rtype     : out  STD_LOGIC;
@@ -50,7 +52,8 @@ component ControlUnit
 end component;
 
 component Dmemory
-	PORT(clk                 : IN 	STD_LOGIC;
+	PORT(
+		  clk                 : IN 	STD_LOGIC;
 		  DMem_address        : IN 	STD_LOGIC_VECTOR(31 DOWNTO 0);
         DMem_write_data	    : IN 	STD_LOGIC_VECTOR(31 DOWNTO 0);
 	     DMemRead            : IN 	STD_LOGIC; 
@@ -58,7 +61,7 @@ component Dmemory
         DMem_read_data	    : OUT 	STD_LOGIC_VECTOR(31 DOWNTO 0);
 		  DMem_out_data		 : OUT 	STD_LOGIC_VECTOR(31 DOWNTO 0);
 		  Data_memory         : out   data_output;
-		  			DMem_addressout        : out 	STD_LOGIC_VECTOR(6 DOWNTO 0)
+		  DMem_addressout        : out 	STD_LOGIC_VECTOR(6 DOWNTO 0)
 );
 END component;
 
@@ -90,30 +93,125 @@ End component;
 signal instruction,NextPC,ReadData1,ReadData2,ALUResult,Write_data,DMemReadData,DMemOutData, reg_arr,SignEx : std_logic_vector(31 downto 0);
 signal ALUop: std_logic_vector(2 downto 0);
 signal dmem_addressout: std_logic_vector(6 downto 0);
-signal RType,LW,SWD,WriteEN,DMemRead,DMemWrite,BEQ,BLT,BNE,clr, Clk, ALUSrc, skip: std_logic;
+signal RType,LW,SWD,WriteEN,DMemRead,DMemWrite,BEQ,BLT,BNE, ALUSrc, skip,clkRC5: std_logic;
+signal clr,start: std_logic:='0';
 Signal datamemory: data_output;
 Signal reg_file: register_output;
 
-begin
+component Hex2LED 
+port (CLK: in STD_LOGIC; X: in STD_LOGIC_VECTOR (3 downto 0); Y: out STD_LOGIC_VECTOR (7 downto 0)); 
+end component; 
 
+type arr is array(0 to 22) of std_logic_vector(7 downto 0);
+signal NAME: arr;
+constant CNTR_MAX : std_logic_vector(23 downto 0) := x"030D40"; --100,000,000 = clk cycles per second
+constant VAL_MAX : std_logic_vector(3 downto 0) := "1001"; --9
+constant RESET_CNTR_MAX : std_logic_vector(17 downto 0) := "110000110101000000";-- 100,000,000 * 0.002 = 200,000 = clk cycles per 2 ms
+signal Cntr : std_logic_vector(26 downto 0) := (others => '0');
+signal Val : std_logic_vector(3 downto 0) := (others => '0');
+signal hexval: std_logic_vector(31 downto 0):=x"0123ABCD";
+signal clk_cntr_reg : std_logic_vector (4 downto 0) := (others=>'0'); 
+
+
+begin
+clkRC5<=btn(0);
 write_data <= DMemReadData when LW='1'
     Else    ALUResult;
-	 
-	 
+led<=sw;
+process(btn,sw)
+	begin
+--		if(btn(0)='1')then
+--			clr<='1';
+--		end if;
+		
+--		if(btn(0)'event and btn(0)='1')then
+--			clk<=1;
+--		end if;
+		if(sw(15)='1') then
+			hexval<=NextPC;
+		end if;
+		
+		if(btn(3)='1') then
+			clr<='0';
+			start<='1';
+		end if;
+		if(btn(1)='1')then
+			hexval<=Datamemory(conv_integer(sw(6 downto 0)));
+		end if;
 
-IDecodePort: IDecode port map(Clk, Instruction,write_data,WriteEN,ReadData1,ReadData2,SignEx,
+		if(btn(2)='1')then
+			hexval<=reg_file(conv_integer(sw(4 downto 0)));
+		end if;
+end process;
+
+timer_counter_process : process (CLK)
+begin
+	if (rising_edge(CLK)) then
+		if ((Cntr = CNTR_MAX) or (BTN(4) = '1')) then
+			Cntr <= (others => '0');
+		else
+			Cntr <= Cntr + 1;
+		end if;
+	end if;
+end process;
+
+--This process increments the digit being displayed on the 
+--7-segment display every second.
+timer_inc_process : process (CLK)
+begin
+	if (rising_edge(CLK)) then
+		if (BTN(4) = '1') then
+			Val <= (others => '0');
+		elsif (Cntr = CNTR_MAX) then
+			if (Val = VAL_MAX) then
+				Val <= (others => '0');
+			else
+				Val <= Val + 1;
+			end if;
+		end if;
+	end if;
+end process;
+
+--This select statement selects the 7-segment diplay anode. 
+with Val select
+	SSEG_AN <= "01111111" when "0001",
+				  "10111111" when "0010",
+				  "11011111" when "0011",
+				  "11101111" when "0100",
+				  "11110111" when "0101",
+				  "11111011" when "0110",
+				  "11111101" when "0111",
+				  "11111110" when "1000",
+				  "11111111" when others;
+
+--This select statement selects the value of HexVal to the necessary
+--cathode signals to display it on the 7-segment
+with Val select
+	SSEG_CA <= NAME(0) when "0001",
+				  NAME(1) when "0010",
+				  NAME(2)when "0011",
+				  NAME(3) when "0100",
+				  NAME(4) when "0101",
+				  NAME(5) when "0110",
+				  NAME(6) when "0111",
+				  NAME(7) when "1000",
+				  NAME(0) when others;
+
+IDecodePort: IDecode port map(ClkRC5, Instruction,write_data,WriteEN,ReadData1,ReadData2,SignEx,
 									   RType,LW,SWD,BLT,BNE,BEQ,reg_arr,skip, reg_file);
-									
 ALUPort: ALU port map(ReadData1,ReadData2, SignEx, ALUop,ALUSrc, ALUResult);
-
-ControlUnitPort: ControlUnit port map(Clk,Instruction,skip,Readdata1,Readdata2,ALUop,
+ControlUnitPort: ControlUnit port map(ClkRC5,Instruction,skip,Readdata1,Readdata2,start,ALUop,
 										NextPC,RType,LW,SWD,WriteEN,DMemRead,clr,DMemWrite,BNE,BLT,BEQ, ALUSrc);
-										
 IFetchPort: IFetch port map(NextPC,Instruction);
-
-DMemoryPort:DMemory port map(Clk, ALUResult,ReadData2,DMemRead,DMemWrite,DMemReadData, DMemOutData, datamemory, DMem_addressout);
-
-
+DMemoryPort:DMemory port map(ClkRC5, ALUResult,ReadData2,DMemRead,DMemWrite,DMemReadData, DMemOutData, datamemory, DMem_addressout);
+CONV1: Hex2LED port map (CLK => CLK, X => HexVal(31 downto 28), Y => NAME(0));
+CONV2: Hex2LED port map (CLK => CLK, X => HexVal(27 downto 24), Y => NAME(1));
+CONV3: Hex2LED port map (CLK => CLK, X => HexVal(23 downto 20), Y => NAME(2));
+CONV4: Hex2LED port map (CLK => CLK, X => HexVal(19 downto 16), Y => NAME(3));		
+CONV5: Hex2LED port map (CLK => CLK, X => HexVal(15 downto 12), Y => NAME(4));
+CONV6: Hex2LED port map (CLK => CLK, X => HexVal(11 downto 8), Y => NAME(5));
+CONV7: Hex2LED port map (CLK => CLK, X => HexVal(7 downto 4), Y => NAME(6));
+CONV8: Hex2LED port map (CLK => CLK, X => HexVal(3 downto 0), Y => NAME(7));
 end Behavioral;
 			 
 
