bc6c37e614d55cfdc3450ad7bbb7482f55b6a515
addy1110 <addy.1110@gmail.com>
2016-12-11 23:30:35 -0500

    updated ControlUnit

--- VHDL Code/Src/RC5/ControlUnit.vhd
+++ VHDL Code/Src/RC5/ControlUnit.vhd
@@ -17,37 +17,37 @@ use ieee.std_logic_arith.all;
 entity ControlUnit is
    Port (   Clk         : in    STD_LOGIC;
     	    Instruction : in    STD_LOGIC_VECTOR(31 DOWNTO 0);
-		    skip		: in    STD_LOGIC;
-		    Read_Data1  : in    STD_LOGIC_VECTOR (31 downto 0);
-		    Read_Data2  : in    STD_LOGIC_VECTOR (31 downto 0);
-			start		: in    STD_LOGIC;
+            skip		: in    STD_LOGIC;
+            Read_Data1  : in    STD_LOGIC_VECTOR (31 downto 0);
+            Read_Data2  : in    STD_LOGIC_VECTOR (31 downto 0);
+            start		: in    STD_LOGIC;
             ALUOp       : out   STD_LOGIC_VECTOR (2 downto 0);
             NextPC      : out   STD_LOGIC_VECTOR (31 downto 0);
             Rtype       : out   STD_LOGIC;
             LW          : out   STD_LOGIC;
-		    SW          : out   STD_LOGIC;
+            SW          : out   STD_LOGIC;
             WriteEn     : out   STD_LOGIC;
             DMemRead    : out   STD_LOGIC;
-		    clr         : in    STD_LOGIC;
+            clr         : in    STD_LOGIC;
             DMemWrite   : out   STD_LOGIC;
             BranchNE    : out   STD_LOGIC;
             BranchLT    : out   STD_LOGIC;
             Branch      : out   STD_LOGIC;
-			ALUSrc      : out   STD_LOGIC);
+            ALUSrc      : out   STD_LOGIC);
 end ControlUnit;
 
 architecture Behavioral of ControlUnit is
 
     SIGNAL R_type, LWD, SWD, BEQ, ADDI, SUBI, ANDI, ORI, BNE, BLT, SHL, SHR, JUMP, R_ADD, R_SUB, R_AND, R_OR, R_NOR, HAL : STD_LOGIC;
-	SIGNAL A, B                                         : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL A, B                                         : STD_LOGIC_VECTOR(31 DOWNTO 0);
     SIGNAL ALU_ROp , ALU_Op                             : STD_LOGIC_VECTOR(2 DOWNTO 0 );
 	SIGNAL Immediate_value_initial                      : STD_LOGIC_VECTOR(15 DOWNTO 0);
 	SIGNAL Immediate_value                              : STD_LOGIC_VECTOR(15 DOWNTO 0);
 	SIGNAL PCIncby1                                     : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL JumpAddress                                  : STD_LOGIC_VECTOR(25 DOWNTO 0);
 	SIGNAL Opcode                                       : STD_LOGIC_VECTOR(5 downto 0);
-    signal numeric_immediate,integer_immediate_N, Imm   : INTEGER;
-    signal NextPCSignal                                 : STD_logic_vector(31 downto 0):=X"00000000";
+    SIGNAL numeric_immediate,integer_immediate_N, Imm   : INTEGER;
+    SIGNAL NextPCSignal                                 : STD_logic_vector(31 downto 0):=X"00000000";
 
 begin
 
@@ -112,22 +112,22 @@ begin
         begin
             IF(((ADDI='1' or LWD ='1' or SWD ='1') and R_type='0') OR (R_ADD ='1' and R_type='1')) then
 		    	ALU_Op <= "000";
-		    ELSIF ((SUBI='1'and R_type='0') OR (R_SUB='1'and R_type='1')) then
-			    ALU_Op <= "001";
-		    ELSIF ((ANDI='1'and R_type='0') OR (R_AND='1'and R_type='1'))then
-			    ALU_Op <= "010";
-		    ELSIF ((ORI='1' and R_type='0') Or (R_OR='1'and R_type='1'))then
-			    ALU_Op <= "011";
-		    ELSIF (R_NOR='1'and R_type='1') then
-			    ALU_Op <= "100";
-		    ELSIF (SHL='1' and R_type='0') then
+            ELSIF ((SUBI='1'and R_type='0') OR (R_SUB='1'and R_type='1')) then
+		        ALU_Op <= "001";
+            ELSIF ((ANDI='1'and R_type='0') OR (R_AND='1'and R_type='1'))then
+		        ALU_Op <= "010";
+            ELSIF ((ORI='1' and R_type='0') Or (R_OR='1'and R_type='1'))then
+                ALU_Op <= "011";
+            ELSIF (R_NOR='1'and R_type='1') then
+	            ALU_Op <= "100";
+            ELSIF (SHL='1' and R_type='0') then
                 ALU_Op <= "101";
-		    ELSIF (SHR='1' and R_type='0') then
+            ELSIF (SHR='1' and R_type='0') then
                 ALU_Op <= "110";
-		    else
-			    ALU_Op <= "111";
-		    END IF;
-	END Process;
+            else
+                ALU_Op <= "111";
+            END IF;
+    END Process;
 	
 	ALUOp <= ALU_Op;
 
@@ -135,21 +135,21 @@ begin
     BranchNE  <= BNE;
     BranchLT  <= BLT;
     SW        <= SWD;
-	LW        <= LWD;
-	Rtype     <= R_type;
-	DMemRead  <= LWD;            -----Read Data memory when load instruction
-	DMemWrite <= SWD;            ----- Write to Data memory
+    LW        <= LWD;
+    Rtype     <= R_type;
+    DMemRead  <= LWD;            -----Read Data memory when load instruction
+    DMemWrite <= SWD;            ----- Write to Data memory
 	        
-	NextPC<=NextPCSignal;
-	ALUSrc <= LWD Or SWD Or ADDI Or SUBI Or ANDI Or ORI Or SHL Or SHR;
+    NextPC<=NextPCSignal;
+    ALUSrc <= LWD Or SWD Or ADDI Or SUBI Or ANDI Or ORI Or SHL Or SHR;
 
-	Process(SWD, BEQ, BLT, BNE, JUMP)
+    Process(SWD, BEQ, BLT, BNE, JUMP)
         begin
             if ((SWD OR BEQ OR BLT OR BNE OR JUMP)='1') then
-		        WriteEn <='0';
-		    else
-			    WriteEn <= '1';
-		    end if;
-	end process;
+                WriteEn <='0';
+            else
+            WriteEn <= '1';
+            end if;
+    end process;
 end Behavioral;
 
