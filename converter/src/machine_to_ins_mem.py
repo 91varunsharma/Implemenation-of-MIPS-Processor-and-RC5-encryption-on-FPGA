af269ac14a374673caeacf588f74e5f672edbf79
addy1110 <addy.1110@gmail.com>
2016-12-12 23:14:08 -0500

    added testbench_1

--- converter/src/machine_to_ins_mem.py
+++ converter/src/machine_to_ins_mem.py
@@ -9,8 +9,8 @@ def get_hex(value):
 
 if __name__ == '__main__':
 
-    read_file = "/Users/ADDY/Google Drive/github/AHD-Project-2016/converter/test/machine/sample_1_machine.txt"
-    write_file = "/Users/ADDY/Google Drive/github/AHD-Project-2016/converter/test//sample_1_ism.txt"
+    read_file = "/Users/ADDY/Google Drive/github/AHD-Project-2016/converter/test/machine/testbench_1_machine.txt"
+    write_file = "/Users/ADDY/Google Drive/github/AHD-Project-2016/converter/test//testbench_1_ism.txt"
     read_from = open(read_file)
     write_to = open(write_file, 'w')
     ins_num = 0
